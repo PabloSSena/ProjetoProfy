d2c7e9df17550eca2d632158c48b2b03e92b21e9
PabloSSena <pablosilvasena@gmail.com>
2020-08-08 00:03:24 -0300

    Feito a opçao pra escolher as materias

--- src/Pages/TeacherForm/index.tsx
+++ src/Pages/TeacherForm/index.tsx
@@ -3,6 +3,8 @@ import PageHeader from '../../Components/PageHeader';
 import './styles.css';
 import Input from '../../Components/Input';
 import warningIcon from '../..//Assets/images/icons/warning.svg'
+import Textarea from '../../Components/Textarea';
+import Select from '../../Components/Select';
 
 function TeacherForm(){
     return(
@@ -20,13 +22,28 @@ function TeacherForm(){
                 <Input name="name" label="Nome completo"/>
                 <Input name="avatar" label="Avatar"/>
                 <Input name="whatsapp" label="Whatsapp"/>
+                <Textarea name="bio" label="Biografia"/>
 
             </fieldset>
 
             <fieldset>
                 <legend>Sobre a aula</legend>
 
-                <Input name="subject" label="Matéria"/>
+                <Select name="subject" label="Matéria" 
+                options={[
+                    {value:'Artes', label:"Artes"},
+                    {value:'Biologia', label:"Biologia"},
+                    {value:'Ciencias', label:"Ciencias"},
+                    {value:'Ed.fisica', label:"Ed.fisica"},
+                    {value:'Geografia', label:"Geografia"},
+                    {value:'História', label:"História"},
+                    {value:'Matemática', label:"Matemática"},
+                    {value:'Quimica', label:"Quimica"},
+
+
+                    
+
+                ]}/>
                 <Input name="cost" label="Custo da sua hora por aula"/>
         
             </fieldset>
