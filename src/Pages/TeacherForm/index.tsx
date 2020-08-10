4e181e0406332c48bb213690a7b3f71c0f24e6ca
PabloSSena <pablosilvasena@gmail.com>
2020-08-09 23:05:23 -0300

    Criando usando estado a maneira de adicionar mais aulas

--- src/Pages/TeacherForm/index.tsx
+++ src/Pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import PageHeader from '../../Components/PageHeader';
 import './styles.css';
 import Input from '../../Components/Input';
@@ -7,11 +7,24 @@ import Textarea from '../../Components/Textarea';
 import Select from '../../Components/Select';
 
 function TeacherForm(){
+
+    const [scheduleItems,setScheduleItems] = useState([
+        {week_day:0,from:'',to:''}
+    ])
+
+    function addNewScheduleItem(){
+
+        setScheduleItems([
+            ...scheduleItems,
+            {week_day:0,from:'',to:''}
+        ]);
+    }
+
     return(
         <div id="page-teacher-form" className="container">
         <PageHeader 
-            title="Que incrivel que você quer dar aulas"
             description="O primeiro passo é preencher esse formulario de inscrição"        
+            title="Que incrivel que você quer dar aulas"
 
         />
 
@@ -30,22 +43,45 @@ function TeacherForm(){
                 <legend>Sobre a aula</legend>
 
                 <Select name="subject" label="Matéria" 
-                options={[
-                    {value:'Artes', label:"Artes"},
-                    {value:'Biologia', label:"Biologia"},
-                    {value:'Ciencias', label:"Ciencias"},
-                    {value:'Ed.fisica', label:"Ed.fisica"},
-                    {value:'Geografia', label:"Geografia"},
-                    {value:'História', label:"História"},
-                    {value:'Matemática', label:"Matemática"},
-                    {value:'Quimica', label:"Quimica"},
-
-
-                    
-
+                    options={[
+                        {value:'Artes', label:"Artes"},
+                        {value:'Biologia', label:"Biologia"},
+                        {value:'Ciencias', label:"Ciencias"},
+                        {value:'Ed.fisica', label:"Ed.fisica"},
+                        {value:'Geografia', label:"Geografia"},
+                        {value:'História', label:"História"},
+                        {value:'Matemática', label:"Matemática"},
+                        {value:'Quimica', label:"Quimica"},
                 ]}/>
-                <Input name="cost" label="Custo da sua hora por aula"/>
-        
+
+                <Input name="cost" label="Custo da sua hora por aula"/>        
+            </fieldset>
+
+            <fieldset>
+                <legend>
+                    <span>Horários disponiveis</span>
+                    <button type='button'onClick={addNewScheduleItem}>+  Novo horário</button>
+                </legend>
+
+                {scheduleItems.map(scheduleItem => {
+                    return (<div key={scheduleItem.week_day}  className="schedule-item">
+                    <Select name="week_day" label="Dia da semana" 
+                        options={[
+                            {value:'0', label:"Domingo"},
+                            {value:'1', label:"Segunda"},
+                            {value:'2', label:"Terça"},
+                            {value:'3', label:"Quarta"},
+                            {value:'4', label:"Quinta"},
+                            {value:'5', label:"Sexta"},
+                            {value:'6', label:"Sabado"},
+                    ]}/>
+
+                    <Input name='from' label='Das' type='time'></Input>
+                    <Input name='to' label='Até' type='time'></Input>
+
+                            
+                </div>)
+                })}
             </fieldset>
 
             <footer>
--- src/Pages/TeacherList/index.tsx
+++ src/Pages/TeacherList/index.tsx
@@ -4,6 +4,7 @@ import TeacherItem from '../../Components/TeacherItem';
 
 import './styles.css';
 import Input from '../../Components/Input';
+import Select from '../../Components/Select';
 
 function TeacherList(){
     return(
@@ -11,8 +12,27 @@ function TeacherList(){
             <PageHeader title ="Estes são os proffys disponiveis">
                 <form id="search-teachers">
                     
-                    <Input name="subject" label="Matéria"/>
-                    <Input name="week_day" label="Dia da semana"/>
+                <Select name="subject" label="Matéria" 
+                    options={[
+                        {value:'Artes', label:"Artes"},
+                        {value:'Biologia', label:"Biologia"},
+                        {value:'Ciencias', label:"Ciencias"},
+                        {value:'Ed.fisica', label:"Ed.fisica"},
+                        {value:'Geografia', label:"Geografia"},
+                        {value:'História', label:"História"},
+                        {value:'Matemática', label:"Matemática"},
+                        {value:'Quimica', label:"Quimica"},
+                ]}/>
+                 <Select name="week_day" label="Dia da semana" 
+                    options={[
+                        {value:'0', label:"Domingo"},
+                        {value:'1', label:"Segunda"},
+                        {value:'2', label:"Terça"},
+                        {value:'3', label:"Quarta"},
+                        {value:'4', label:"Quinta"},
+                        {value:'5', label:"Sexta"},
+                        {value:'6', label:"Sabado"},
+                ]}/>
                     <Input type="time" name="time" label="Hora"/>
 
                 </form>
