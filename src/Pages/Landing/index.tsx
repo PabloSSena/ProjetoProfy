54c459f25ccafd604ed0a9594abc5be216ff42fc
PabloSSena <pablosilvasena@gmail.com>
2020-08-09 23:30:07 -0300

    Usando o banco de dados pra aparecer total de conexoes

--- src/Pages/Landing/index.tsx
+++ src/Pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import {Link} from 'react-router-dom';
 import logoImg from '../../Assets/images/logo.svg';
 import landingIgm from '../../Assets/images/landing.svg';
@@ -6,12 +6,22 @@ import studyIcon from '../../Assets/images/icons/study.svg';
 import giveClassesIcon from '../../Assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../Assets/images/icons/purple-heart.svg';
 import './styles.css';
+import api from '../../Services/api';
 
 
 
 
 
 function Landing(){
+    const [totalCOnnections,setTotalConnections] = useState(0)
+
+    useEffect(()=>{
+        api.get('connections').then(response => {
+            const {total} = response.data;
+            setTotalConnections(total)
+        })
+    },[])
+
     return(
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -35,7 +45,7 @@ function Landing(){
                     </Link>
                 </div>
                 <span className="total-connections">
-                    total de 200 conexões ja realizadas <img src={purpleHeartIcon} alt="Coraçao roxo"/>
+                    total de {totalCOnnections} conexões ja realizadas <img src={purpleHeartIcon} alt="Coraçao roxo"/>
                 </span>
             </div>
         </div>
